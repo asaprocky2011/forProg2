507343d179ae75424681c1ee02e5d04524bfc3c0
konan <alberticca@gmail.com>
2021-12-24 05:49:32 +0300

    начало второй лабы

---
+++ second_lab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    // uint1024_t a;
+    // uint1024_t b, v;
+    // scanf_value(&a);
+    // scanf_value(&b);
+    // printf("multiplication: ");
+    // v = mult_op(a, b);
+    // printf_value(mult_op(a, b));
+    // printf("\ndifference: ");
+    // printf_value(subtr_op(a, b));
+    // printf("\namount: ");
+    // printf_value(add_op(a, b));
+    return 0;
+}
+
